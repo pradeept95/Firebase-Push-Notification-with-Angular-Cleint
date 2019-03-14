8bc73d4b854cf81ea638b392133ee9c3f1565c4e
Pradeep Raj Thapaliya <thapaliyapradeep@gmail.com>
2019-03-14 11:27:07 +0545

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyDqyCMEcKjsM2F2heenGuFJGyaBS0QYR9g",
-    authDomain: "manifacturinginfosystem.firebaseapp.com",
+    apiKey: "****",
+    authDomain: "***.firebaseapp.com",
     databaseURL: "https://manifacturinginfosystem.firebaseio.com",
-    projectId: "manifacturinginfosystem",
-    storageBucket: "manifacturinginfosystem.appspot.com",
-    messagingSenderId: "663402776868"
+    projectId: "****",
+    storageBucket: "****.appspot.com",
+    messagingSenderId: "****"
   }
 };
